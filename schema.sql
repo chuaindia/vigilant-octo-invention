38f5183130655d0080d8489b77322e41cbece278
Tanusri Ghosh <tghosh25@gmail.com>
2023-04-05 16:16:30 +0530

    Initialize feature_branch_day_4_SQL

---
+++ schema.sql
@@ -0,0 +1,55 @@
+/* Database schema to keep the structure of entire database. */
+
+CREATE TABLE animals(
+   id integer NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name varchar(100), date_of_birth date, escape_attempts integer, 
+	neutered boolean, weight_kg decimal);
+
+/* Day2 */
+
+ALTER TABLE animals
+  ADD species varchar(40);
+
+/* Day3 */
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name varchar(255),
+    age integer
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name varchar(255)
+);
+
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD species_id INTEGER REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INTEGER REFERENCES owners(id);    
+
+
+/* Day4 */
+
+CREATE TABLE vets (
+
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    age INTEGER,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+
+    id SERIAL PRIMARY KEY,
+    vets_id INTEGER REFERENCES vets(id) ON DELETE CASCADE,
+	species_id INTEGER REFERENCES species(id) ON DELETE CASCADE
+);
+
+CREATE TABLE visits (
+    vets_id INTEGER REFERENCES vets(id) ON DELETE CASCADE,
+	animals_id INTEGER REFERENCES animals(id) ON DELETE CASCADE,
+	date_of_visit DATE
+);
+
+
